1755e14859c706abef5903d0dff6e964142aa219
lorencr7 <eden__lorenzo@hotmail.com>
2014-04-04 17:21:50 +0200

    Initial Commit

---
+++ Pitagorin/Pitagorin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pitagorin
+//
+//  Created by Lorenzo Villarroel Pérez on 04/04/14.
+//  Copyright (c) 2014 Lorenzov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pitagorin/PitagorinTests/PitagorinTests.m
@@ -0,0 +1,34 @@
+//
+//  PitagorinTests.m
+//  PitagorinTests
+//
+//  Created by Lorenzo Villarroel Pérez on 04/04/14.
+//  Copyright (c) 2014 Lorenzov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PitagorinTests : XCTestCase
+
+@end
+
+@implementation PitagorinTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
